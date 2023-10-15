51c7109d437b1745b62fb4e0e4251a2f5252577d
tushar <tushar158gupta@gmail.com>
2023-10-15 21:22:48 +0530

    hello again

--- src/app/utils/connect.jsx
+++ src/app/utils/connect.jsx
@@ -1,6 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+let prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
 
-const prisma = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV === "production") globalThis.prisma = client;
-
-export default prisma;
+export default prisma
